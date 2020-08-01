40a196c030404620ef828baf10c81715f533256b
ajeet <imajeet5@gmail.com>
2020-08-01 22:42:09 +0530

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(int argc, char const *argv[])
+{
+    int num1;
+    int num2;
+    double num3;
+
+    cout << "Enter a integer"<<endl;
+    cin >> num1;
+    cout << "You Entered: " << num1 << endl;
+
+    return 0;
+}
